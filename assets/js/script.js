dc353e63a703c9a02c013686044152b21b399a77
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-10-13 15:55:23 +1100

    Added modal contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,29 @@
-$hamburgerClk = $(".hamburger");
-$smallMenuClk = $("#smallMenuItems");
+let $hamburgerClk = $(".hamburger");
+let $smallMenuClk = $("#smallMenuItems");
+let $stuarts = $("#stuarts")
+let $modalClose = $("#modalClose");
+let $modalContainer = $(".modalContainer");
+let $modalCard = $(".modalCard");
+let $contactBtn = $("button")
 let smallMenuToggle = false;
 
-$hamburgerClk.click(() => {
-  if ($smallMenuClk.hasClass("hide")) {
-    $smallMenuClk.removeClass("hide");
-  } else {
-    $smallMenuClk.addClass("hide");
-  }
+$hamburgerClk.click(() => $smallMenuClk.toggleClass("hide"));
+$smallMenuClk.click(() => $smallMenuClk.addClass("hide"));
+
+$modalCard.on("click", (event) => {
+    event.stopPropagation();
+    console.log(event.target)
+    if (event.target.id === $modalClose[0].id){
+      $(".modalForm")[0].reset();
+      $modalContainer.toggleClass("hide");
+    }
+})
+
+$modalContainer.click((event) => $modalContainer.toggleClass("hide"));
+
+$contactBtn.click((event) => {
+  $modalContainer.toggleClass("hide")
+  $(".modalForm")[0].reset();
 });
 
-$smallMenuClk.click(() => {
-  $smallMenuClk.addClass("hide");
-});
+$(".modalForm").submit((event) => $modalContainer.toggleClass("hide"));
